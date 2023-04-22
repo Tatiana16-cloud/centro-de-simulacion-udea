985e21d8dfa1eb8264902bbe6b577f5ef8b65b66
Juan Velasquez <33909059+jdvr1994@users.noreply.github.com>
2023-04-22 15:29:57 -0500

    add circle to table component

--- src/components/Table/TableRow.component.jsx
+++ src/components/Table/TableRow.component.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import './table.css'
+import Circle from '../Circle/circle.component'
+
+const statusMapping = {
+    pending: '#bb3333',
+    active: '#40bb30',
+    blocked: '#33ccff',
+    inactive: 'yellow'
+}
+
+const objectKeys = Object.keys(statusMapping);
 
 const TableRow = ({rowData,onEditEvent, onDeleteEvent}) => {
     return (
         <tr>
-            {Object.keys(rowData).map((key, index) => (
-                <td className='td' key={index}>{rowData[key]}</td>
-            ))}
+            {Object.keys(rowData).map((key, index) => {
+                if(key==='status') 
+                    return <td className='td' key={index}>
+                                <Circle 
+                                    color={statusMapping[objectKeys[Math.floor(Math.random() * objectKeys.length)]]} 
+                                    size={30}
+                                />
+                            </td>
+                return (<td className='td' key={index}>{rowData[key]}</td>)
+            })}
             <td className='td actions'>
                 <button className='action-button' onClick={()=> onEditEvent(rowData)}>Editar</button>
                 <button className='action-button' onClick={()=> onDeleteEvent(rowData.id)}>Eliminar</button>
