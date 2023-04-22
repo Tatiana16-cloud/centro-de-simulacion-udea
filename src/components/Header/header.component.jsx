793073aa608586998af18bdae8ceff4657aa43f3
Juan Velasquez <33909059+jdvr1994@users.noreply.github.com>
2023-04-22 14:54:35 -0500

    header styles

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
   const title = 'Inventario'
+  const user = {
+    name: "Pepito Perez"
+  }
+
   return (
     <div className='header'>
       {/* Contenido del header */}
       <h2 className='title'>{title}</h2>
+      <div className='user-info'>
+        <FontAwesomeIcon icon={faUser} style={{ fontSize: "20px"}}/>
+        <h2 className='text-name'>{user.name}</h2>
+      </div>
     </div>
   );
 };
